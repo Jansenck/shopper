d78210cae053063d709d59661b590f2906922cfc
Jansenck <jansencaik@gmail.com>
2023-09-10 22:50:34 -0300

    chore: fix documentation

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,15 +1,11 @@
-import "reflect-metadata";
-import "express-async-errors";
 import express, { Express } from "express";
 import cors from "cors";
 
-import { loadEnv, connectDb, disconnectDB } from "@/config";
+import { connectDb, disconnectDB } from "@/config";
 
 import { validateBody } from "./middlewares/validation-middleware";
 import { fileValidationSchema } from "./schemas/file-schema";
 import { postUpdateProductPrice, getProducts } from "./controllers/file-controller";
-
-loadEnv();
 
 const app = express();
 app
